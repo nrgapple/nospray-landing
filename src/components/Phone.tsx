f9a081bc011b583ac1eddf87395a7ddb4a969bae
nrgapple <champtoday@gmail.com>
2020-07-22 20:19:52 -0400

    remove bottom

--- src/components/Phone.tsx
+++ src/components/Phone.tsx
@@ -11,11 +11,6 @@ const Phone = ({ children }: Props) => (
     <span className="speaker" title="Speaker"></span>
     <span className="camera" title="Camera"></span>
     <span className="proximity" title="Proximity Sensor"></span>
-
-    <button className="silence" title="Silence Switch"></button>
-    <button className="volume-up" title="Volume Up"></button>
-    <button className="volume-down" title="Volume Down"></button>
-    <button className="sleep" title="Sleep"></button>
   </div>
 )
 
