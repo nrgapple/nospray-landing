5339a1769f72656f6b8313ea3f0fabed52edef98
nrgapple <champtoday@gmail.com>
2020-07-27 21:42:41 -0400

    add call

--- src/pages/api/license/index.ts
+++ src/pages/api/license/index.ts
@@ -34,21 +34,42 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     data = response
   })
   await navigationPromise
-  await page.waitForSelector("#table2")
+  try {
+    await page.waitForSelector("#table2", { timeout: 3000 })
+  } catch (e) {
+    console.log("timeout")
+    res.status(403).end()
+  }
+  //page.waitForSelector("#ctl00_cphResults_vsSummary"),
   const element = await page.$(
     "#ctl00_cphResults_rptApplicators_ctl00_CertificationID"
   )
   if (!element) {
     res.status(403).end()
-  }
+  } else {
+    const text = await page.evaluate(
+      (element: any) => element.textContent,
+      element
+    )
+    if (id === text) {
+      console.log("success")
+    }
+    await browser.close()
+    const resp = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}?key=${process.env.NEXT_PUBLIC_API_KEY}`,
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: 
+          plan: plan?.toUpperCase(),
+        }),
+      }
+    )
 
-  const text = await page.evaluate(
-    (element: any) => element.textContent,
-    element
-  )
-  if (id === text) {
-    console.log("success")
+    res.send(text)
   }
-  await browser.close()
-  res.send(text)
 }
